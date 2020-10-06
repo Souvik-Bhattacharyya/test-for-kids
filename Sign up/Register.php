e613d744bede6232dbfe641c36035ba33430182b
Souvik2001 <57744600+Souvik2001@users.noreply.github.com>
2020-10-06 03:52:51 -0700

    My Kids Exam Test Website

---
+++ Sign up/Register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NEW ACCOUNT</title>
+	<link rel="stylesheet" type="text/css" href="Style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+<!--  LOGIN FORM  -->
+<div class="login-box">
+	<h1>REGISTER</h1>
+<!--  Username & Password   -->
+	 <div class="textbox">
+	 	<i class="glyphicon glyphicon-envelope"></i>
+ 		<input type="text" name="@gmail.com"  placeholder="Email">
+	 </div>
+	 <div class="textbox">
+	 	<i class="glyphicon glyphicon-user"></i>
+ 		<input type="text" name="Username"  placeholder="Username">
+	 </div>
+	 <div class="textbox">
+ 		<i class="glyphicon glyphicon-lock"></i>
+		<input type="password" name="Password" placeholder="Password">
+	 </div>
+	 <div class="textbox">
+ 		<i class="glyphicon glyphicon-eye-open"></i>
+		<input type="text" name="Password" placeholder="Confirm Password">
+	 </div>
+<!--LOGIN & SIGN UP-->
+	 <div>
+		<a href="Data/Exam.html">
+			<button class="button"><span>SIGN UP</span></button>
+		</a>
+	 </div>
+	 <div class="bottom-text">
+	 	Already a Member
+	 	<a href="../index.html">Login ?</a>
+	 </div>
+
+</div>
+</form>
+</body> 
+</html>
